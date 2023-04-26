6da6483998ee60c72d6b68a283895d409a2bdfd4
phungo-hub <ngokieuphu@gmail.com>
2023-04-27 01:25:46 +0700

    fix (env file) add env

--- src/services/article.ts
+++ src/services/article.ts
@@ -1,13 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// const apiKey = process.env.API_ARTICLE_KEY;
+const apiKey = process.env.API_ARTICLE_KEY;
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com",
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', "ab5b0d6760mshaf8c098925f9f0cp1c171bjsn0fcec552ad15")
+            if (apiKey){
+                headers.set('X-RapidAPI-Key', apiKey)
+            }
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com')
         },
     }),
